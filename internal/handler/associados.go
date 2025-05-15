d73182466be2517ae2d195444f7c1b707e34bd7c
Eduardo Silva <eduardofox989@protonmail.com>
2025-05-15 16:48:03 -0300

    added: get to associated

--- internal/handler/associados.go
+++ internal/handler/associados.go
@@ -2,24 +2,77 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"chamada-pagamento-system/db"
 	"chamada-pagamento-system/internal/domain"
 )
 
+func getAssociated(w http.ResponseWriter, _ *http.Request) {
+	var assoc []domain.Associated
+	if err := db.DB.Find(&assoc).Error; err != nil {
+		http.Error(w, "erro ao listar assoc: "+err.Error(), http.StatusBadRequest)
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	if len(assoc) == 0 {
+		http.Error(w, "nenhum associado encontrado", http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(assoc)
+}
+
 func createAssociated(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "metodo não permitido", http.StatusMethodNotAllowed)
+	}
+
 	var assoc domain.Associated
+
 	if err := json.NewDecoder(r.Body).Decode(&assoc); err != nil {
 		http.Error(w, "JSON invalido: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	if err := db.DB.Create(&assoc); err != nil {
-		http.Error(w, "Falha ao salvar dados no banco de dados", http.StatusBadRequest)
+	if err := assoc.IsValid(); err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+
+		resposta := map[string][]string{
+			"erros": err,
+		}
+
+		json.NewEncoder(w).Encode(resposta)
+
+		return
+	}
+
+	if err := db.DB.Create(&assoc).Error; err != nil {
+		http.Error(
+			w,
+			"Falha ao salvar dados no banco de dados: "+err.Error(),
+			http.StatusBadRequest,
+		)
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(assoc)
+}
+
+func MapeamentoAssoc(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getAssociated(w, r)
+	case http.MethodPost:
+		createAssociated(w, r)
+	case http.MethodPut:
+		fmt.Fprintln(w, "You made a PUT request!")
+	case http.MethodDelete:
+		fmt.Fprintln(w, "You made a DELETE request!")
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 }
--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -4,13 +4,13 @@ import (
 	"net/http"
 )
 
-func RegisterHandlers(mux *http.ServeMux) {
-	mux.HandleFunc("/ping", pingPong)
-	mux.HandleFunc("/associated", createAssociated)
-}
-
 func pingPong(w http.ResponseWriter, _ *http.Request) {
 	if _, err := w.Write([]byte("pong")); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
+}
+
+func RegisterHandlers(mux *http.ServeMux) {
+	mux.HandleFunc("/ping", pingPong)
+	mux.HandleFunc("/associated", MapeamentoAssoc)
 }
