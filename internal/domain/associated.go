87388bd8ce9a085710c6ce41a944a103e2b60179
Eduardo Silva <eduardofox989@protonmail.com>
2025-05-15 16:46:05 -0300

    added: validation to fields to associated

--- internal/domain/associated.go
+++ internal/domain/associated.go
@@ -1,6 +1,11 @@
 package domain
 
-import "gorm.io/gorm"
+import (
+	"reflect"
+	"strings"
+
+	"gorm.io/gorm"
+)
 
 type MaritalStatus string
 
@@ -12,8 +17,25 @@ const (
 
 type Associated struct {
 	gorm.Model
-	Cpf           string        `json:"cpf"`
-	Name          string        `json:"name"`
-	DateBirth     string        `json:"dateBirth"`
-	MaritalStatus MaritalStatus `json:"maritalStatus"`
+	CPF           string        `json:"cpf"           gorm:"no null;unique"`
+	Name          string        `json:"name"          gorm:"not null"`
+	DateBirth     string        `json:"dateBirth"     gorm:"not null"`
+	MaritalStatus MaritalStatus `json:"maritalStatus" gorm:"not null"`
+}
+
+func (a Associated) IsValid() []string {
+	var error []string
+	v := reflect.ValueOf(a)
+	t := reflect.TypeOf(a)
+
+	for i := range v.NumField() {
+		fieldValue := v.Field(i)
+		fieldName := t.Field(i).Name
+
+		if fieldValue.Kind() == reflect.String && strings.TrimSpace(fieldValue.String()) == "" {
+			error = append(error, "campo "+fieldName+" esta vazio")
+		}
+
+	}
+	return error
 }
