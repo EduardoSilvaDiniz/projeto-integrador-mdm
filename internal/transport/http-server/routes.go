2acb34999ee1e544c16b090c79789be41a259cda
Eduardo Silva <eduardofox989@protonmail.com>
2025-05-17 13:57:09 -0300

    change: move and rename handlers file

--- internal/transport/http-server/routes.go
+++ internal/transport/http-server/routes.go
@@ -1,1 +1,18 @@
 package httpserver
+
+import (
+	"net/http"
+
+	"chamada-pagamento-system/internal/transport/http-server/handlers"
+)
+
+func pingPong(w http.ResponseWriter, _ *http.Request) {
+	if _, err := w.Write([]byte("pong")); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+}
+
+func RegisterHandlers(mux *http.ServeMux) {
+	mux.HandleFunc("/ping", pingPong)
+	mux.HandleFunc("/associated", handlers.MapEndpointsToAssoc)
+}
