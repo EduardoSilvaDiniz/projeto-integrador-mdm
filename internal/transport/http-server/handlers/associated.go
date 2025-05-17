3e8584c074e41c003728b6e2e7f5cbf313707c71
Eduardo Silva <eduardofox989@protonmail.com>
2025-05-17 14:17:14 -0300

    refactoring: rename vars and changing json.encode() to fmt.Fprintln() to return response

--- internal/transport/http-server/handlers/associated.go
+++ internal/transport/http-server/handlers/associated.go
@@ -10,67 +10,58 @@ import (
 	"chamada-pagamento-system/internal/transport/http-server/dto"
 )
 
-var assocEntitie entities.Associated
+var assocEntity entities.Associated
 
 func getAssociated(w http.ResponseWriter, _ *http.Request) {
-	var assoc []dto.Associated
+	var assocDtoList []dto.Associated
 
-	if err := migrations.DB.Find(&assoc).Error; err != nil {
+	if err := migrations.DB.Find(&assocDtoList).Error; err != nil {
 		http.Error(w, "erro ao listar assoc: "+err.Error(), http.StatusBadRequest)
 	}
-	w.Header().Set("Content-Type", "application/json")
-
-	if len(assoc) == 0 {
+	if len(assocDtoList) == 0 {
 		http.Error(w, "nenhum associado encontrado", http.StatusNotFound)
 		return
 	}
 
-	json.NewEncoder(w).Encode(assoc)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, assocDtoList)
 }
 
 func createAssociated(w http.ResponseWriter, r *http.Request) {
-	var assoc dto.Associated
+	var assocDto dto.Associated
 
-	if err := json.NewDecoder(r.Body).Decode(&assoc); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&assocDto); err != nil {
 		http.Error(w, "JSON invalido: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	if err := assoc.IsValid(); err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-
-		resposta := map[string][]string{
+	if err := assocDto.IsValid(); err != nil {
+		response := map[string][]string{
 			"erros": err,
 		}
 
-		json.NewEncoder(w).Encode(resposta)
-
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, response)
 		return
 	}
-
-	if err := migrations.DB.Create(&assoc).Error; err != nil {
-		http.Error(
-			w,
-			"Falha ao salvar dados no banco de dados: "+err.Error(),
-			http.StatusBadRequest,
-		)
+	result := migrations.DB.Create(&assocDto)
+	if err := result.Error; err != nil {
+		http.Error(w, "Falha na gravação dos dados: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(assoc)
+	fmt.Fprintln(w, assocDto)
 }
 
 func deleteAssoc(w http.ResponseWriter, r *http.Request) {
 	var assoc dto.Associated
 	if err := json.NewDecoder(r.Body).Decode(&assoc); err != nil {
-		http.Error(w, "erro ao pega o objeto"+err.Error(), http.StatusBadRequest)
+		http.Error(w, "falha na decotificação do json: "+err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	result := migrations.DB.Where("name = ?", assoc.Name).Delete(&assocEntitie)
-
+	result := migrations.DB.Where("name = ?", assoc.Name).Delete(&assocEntity)
 	if err := result.Error; err != nil {
 		http.Error(w, "erro ao remover "+assoc.Name+": "+err.Error(), http.StatusBadRequest)
 		return
@@ -82,7 +73,7 @@ func deleteAssoc(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Associado deletado com sucesso"))
+	fmt.Fprintln(w, "Associado deletado com sucesso")
 }
 
 func MapEndpointsToAssoc(w http.ResponseWriter, r *http.Request) {
