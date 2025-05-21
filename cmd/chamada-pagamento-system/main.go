4370839d4bdc611e7b45ef8f8e7da3f59bc7ceec
Eduardo Silva <eduardofox989@protonmail.com>
2025-05-21 17:45:21 -0300

    added: pgx and function to connect database
    gorm was used in the beginning because it was easier, but now that the
    development of the project has started to take a more professional form,
    I'm making the decision to migrate to a less magical and more flexible
    database management.

--- cmd/chamada-pagamento-system/main.go
+++ cmd/chamada-pagamento-system/main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
-
-	httpserver "chamada-pagamento-system/internal/transport/http-server"
+	"chamada-pagamento-system/internal/infra/repositories"
+	"chamada-pagamento-system/tutorial"
+	"context"
+	"fmt"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	httpserver.RegisterHandlers(mux)
+	// mux := http.NewServeMux()
+	// httpserver.RegisterHandlers(mux)
 
 	// migrations.PostgresMigrate()
 	// migrations.DB.Migrator().DropTable(&entities.Associated{})
 	// migrations.DB.AutoMigrate(&entities.Associated{})
 
-	log.Println("servidor inicializado em :8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
-		log.Fatal("Erro ao iniciar servidor:", err)
+	// log.Println("servidor inicializado em :8080")
+	// if err := http.ListenAndServe(":8080", mux); err != nil {
+	// 	log.Fatal("Erro ao iniciar servidor:", err)
+	// }
+	db := repositories.PgxConnect()
+	queries := tutorial.New(db)
+	ctx := context.Background()
+
+	err := queries.CreateAssoc(ctx, tutorial.CreateAssocParams{
+		Cpf:           123,
+		Name:          "edu",
+		DateBirth:     "1010",
+		MaritalStatus: "lala",
+	})
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+		return
+	}
+
+	err = queries.GetAssoc(ctx)
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+		return
 	}
 }
--- internal/infra/repositories/infra.go
+++ internal/infra/repositories/infra.go
@@ -1,14 +1,17 @@
 package repositories
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/jackc/pgx/v5"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var config = "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
-
 
 func PostgresMigrate() *gorm.DB {
 	database, err := gorm.Open(postgres.Open(config), &gorm.Config{})
@@ -16,4 +19,22 @@ func PostgresMigrate() *gorm.DB {
 		log.Fatal("erro ao conectar ao banco de dados", err)
 	}
 	return database
+}
+
+var dbURL string = fmt.Sprintf(
+	"postgresql://%s:%s@%s:%s/%s",
+	"postgres",
+	"postgres",
+	"localhost",
+	"5432",
+	"postgres",
+)
+
+func PgxConnect() *pgx.Conn {
+	conn, err := pgx.Connect(context.Background(), dbURL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database %v\n", err)
+		os.Exit(1)
+	}
+	return conn
 }
