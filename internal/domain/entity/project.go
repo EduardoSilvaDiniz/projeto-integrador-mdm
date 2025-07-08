13e0165e627d1d4f091aa5e28f7a9afc0199420a
Eduardo Silva <eduardofox989@protonmail.com>
2025-07-08 15:08:15 -0300

    delete: unnecessary code and files

--- internal/domain/entity/project.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-import "github.com/google/uuid"
-
-type Project struct {
-	ID            uuid.UUID `json:"id"`
-	Name          string    `json:"name,omitempty"`
-	PaymentAmount float32   `json:"payment_amount,omitempty"`
-}
