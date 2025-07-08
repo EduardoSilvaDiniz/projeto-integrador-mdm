22159de4b102cc8b07389e902c21b4bfa8b00898
Eduardo Silva <eduardofox989@protonmail.com>
2025-07-08 16:47:37 -0300

    change: meeting entity

--- internal/domain/entity/meeting.go
+++ internal/domain/entity/meeting.go
@@ -1,12 +1,7 @@
 package entity
 
-import "github.com/google/uuid"
-
 type Meeting struct {
-	ID        uuid.UUID `json:"id"`
-	ProjectId int       `json:"project_id,omitempty"`
-	GroupId   int       `json:"group_id,omitempty"`
-	HubId     int       `json:"hub_id,omitempty"`
-	Date      string    `json:"date,omitempty"`
-	Time      string    `json:"time,omitempty"`
+	ID        int64  `json:"id"`
+	Date      string `json:"date,omitempty"`
+	Time      string `json:"time,omitempty"`
 }
