10cf5ef6210b442a2e5e66e2be4e45a1be7749da
Eduardo Silva <eduardofox989@protonmail.com>
2025-07-10 15:29:30 -0300

    added: implement service presence

--- internal/service/presence.go
+++ internal/service/presence.go
@@ -2,15 +2,18 @@ package service
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"io"
+
 	"projeto-integrador-mdm/internal/database"
 	"projeto-integrador-mdm/internal/domain"
 )
 
 type PresenceService interface {
-	Create(ctx context.Context, body io.ReadCloser) (*domain.Presence, error)
-	// List(ctx context.Context) ([]database.Present, error)
-	// Delete(ctx context.Context, numberCard string) (int64, error)
+	Create(ctx context.Context, body io.ReadCloser) (*database.CreatePresenceParams, error)
+	List(ctx context.Context) ([]database.Presence, error)
+	Delete(ctx context.Context, body io.ReadCloser) (*database.DeletePresenceByCompositeKeyParams, error)
 }
 
 type presenceService struct {
@@ -21,7 +24,54 @@ func NewPresenceService(queries *database.Queries) *presenceService {
 	return &presenceService{repo: queries}
 }
 
-func (s *presenceService) Create(ctx context.Context, body io.ReadCloser) (*domain.Presence, error) {
-	// s.repo.CreatePresence(ctx, arg database.CreatePresenceParams)
-	return nil, nil
+func (s *presenceService) List(ctx context.Context) ([]database.Presence, error) {
+	return s.repo.GetPresence(ctx)
+}
+
+func (s *presenceService) Create(ctx context.Context, body io.ReadCloser) (*database.CreatePresenceParams, error) {
+	var dto domain.Presence
+	if err := json.NewDecoder(body).Decode(&dto); err != nil {
+		return nil, err
+	}
+
+	if err := IsValid(dto); err != nil {
+		return nil, err
+	}
+
+	params := dto.ToCreateParams()
+
+	if err := s.repo.CreatePresence(ctx, params); err != nil {
+		return nil, err
+	}
+	return &params, nil
+}
+
+// number_card = ? AND meeting_id = ?
+func (s *presenceService) Delete(ctx context.Context, body io.ReadCloser) (*database.DeletePresenceByCompositeKeyParams, error) {
+	var dto domain.PresenceCompositeKey
+	if err := json.NewDecoder(body).Decode(&dto); err != nil {
+		return nil, err
+	}
+
+	if err := IsValid(dto); err != nil {
+		return nil, err
+	}
+
+	params := dto.ToCreateParams()
+
+	result, err := s.repo.DeletePresenceByCompositeKey(ctx, params)
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return nil, err
+	}
+
+	if rows == 0 {
+		return nil, errors.New("não foi encontrando registros")
+	}
+
+	return &params, nil
 }
