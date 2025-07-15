89a754c44a27db328dd37be8808f32554a9ad98d
Eduardo Silva <eduardofox989@protonmail.com>
2025-07-15 16:30:24 -0300

    rename database to db

--- internal/service/payment.go
+++ internal/service/payment.go
@@ -3,31 +3,34 @@ package service
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"io"
-	"projeto-integrador-mdm/internal/database"
+	"projeto-integrador-mdm/internal/db"
 	"projeto-integrador-mdm/internal/domain"
 	"strconv"
 )
 
 type PaymentService interface {
-	Create(ctx context.Context, body io.ReadCloser) (*database.CreatePaymentParams, error)
-	List(ctx context.Context) ([]database.Payment, error)
+	Create(ctx context.Context, body io.ReadCloser) (*db.CreatePaymentParams, error)
+	GetById(ctx context.Context, id string) (*db.Payment, error)
+	Update(ctx context.Context, body io.ReadCloser) (*db.UpdatePaymentParams, error)
+	List(ctx context.Context) ([]db.Payment, error)
 	Delete(ctx context.Context, id string) (int64, error)
 }
 
 type paymentService struct {
-	repo *database.Queries
+	repo *db.Queries
 }
 
-func NewPaymentService(queries *database.Queries) *paymentService {
+func NewPaymentService(queries *db.Queries) *paymentService {
 	return &paymentService{repo: queries}
 }
 
-func (s *paymentService) List(ctx context.Context) ([]database.Payment, error) {
+func (s *paymentService) List(ctx context.Context) ([]db.Payment, error) {
 	return s.repo.GetPayment(ctx)
 }
 
-func (s *paymentService) Create(ctx context.Context, body io.ReadCloser) (*database.CreatePaymentParams, error) {
+func (s *paymentService) Create(ctx context.Context, body io.ReadCloser) (*db.CreatePaymentParams, error) {
 	var dto domain.Payment
 	if err := json.NewDecoder(body).Decode(&dto); err != nil {
 		return nil, err
@@ -41,6 +44,46 @@ func (s *paymentService) Create(ctx context.Context, body io.ReadCloser) (*datab
 	if err := s.repo.CreatePayment(ctx, params); err != nil {
 		return nil, err
 	}
+	return &params, nil
+}
+
+func (s *paymentService) GetById(ctx context.Context, id string) (*db.Payment, error) {
+	idInt, err := strconv.ParseInt(id, 10, 32)
+	if err != nil {
+		return nil, err
+	}
+
+	register, err := s.repo.GetPaymentByID(ctx, idInt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &register, nil
+}
+
+func (s *paymentService) Update(ctx context.Context, body io.ReadCloser) (*db.UpdatePaymentParams, error) {
+	var dto domain.Payment
+
+	if err := json.NewDecoder(body).Decode(&dto); err != nil {
+		return nil, err
+	}
+
+	params := dto.ToUpdateParams()
+
+	result, err := s.repo.UpdatePayment(ctx, params)
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return nil, err
+	}
+
+	if rows == 0 {
+		return nil, errors.New("not found")
+	}
+
 	return &params, nil
 }
 
--- internal/service/presence.go
+++ internal/service/presence.go
@@ -6,29 +6,31 @@ import (
 	"errors"
 	"io"
 
-	"projeto-integrador-mdm/internal/database"
+	"projeto-integrador-mdm/internal/db"
 	"projeto-integrador-mdm/internal/domain"
 )
 
 type PresenceService interface {
-	Create(ctx context.Context, body io.ReadCloser) (*database.CreatePresenceParams, error)
-	List(ctx context.Context) ([]database.Presence, error)
-	Delete(ctx context.Context, body io.ReadCloser) (*database.DeletePresenceByCompositeKeyParams, error)
+	Create(ctx context.Context, body io.ReadCloser) (*db.CreatePresenceParams, error)
+	GetById(ctx context.Context, body io.ReadCloser) (*db.Presence, error)
+	Update(ctx context.Context, body io.ReadCloser) (*db.UpdatePresenceParams, error)
+	List(ctx context.Context) ([]db.Presence, error)
+	Delete(ctx context.Context, body io.ReadCloser) (*db.DeletePresenceByCompositeKeyParams, error)
 }
 
 type presenceService struct {
-	repo *database.Queries
+	repo *db.Queries
 }
 
-func NewPresenceService(queries *database.Queries) *presenceService {
+func NewPresenceService(queries *db.Queries) *presenceService {
 	return &presenceService{repo: queries}
 }
 
-func (s *presenceService) List(ctx context.Context) ([]database.Presence, error) {
+func (s *presenceService) List(ctx context.Context) ([]db.Presence, error) {
 	return s.repo.GetPresence(ctx)
 }
 
-func (s *presenceService) Create(ctx context.Context, body io.ReadCloser) (*database.CreatePresenceParams, error) {
+func (s *presenceService) Create(ctx context.Context, body io.ReadCloser) (*db.CreatePresenceParams, error) {
 	var dto domain.Presence
 	if err := json.NewDecoder(body).Decode(&dto); err != nil {
 		return nil, err
@@ -46,9 +48,55 @@ func (s *presenceService) Create(ctx context.Context, body io.ReadCloser) (*data
 	return &params, nil
 }
 
+func (s *presenceService) GetById(ctx context.Context, body io.ReadCloser) (*db.Presence, error) {
+	var dto domain.PresenceByCompositeKey
+	if err := json.NewDecoder(body).Decode(&dto); err != nil {
+		return nil, err
+	}
+
+	if err := IsValid(dto); err != nil {
+		return nil, err
+	}
+
+	params := dto.ToCreateParams()
+
+	register, err := s.repo.GetPresenceByCompositeKey(ctx, db.GetPresenceByCompositeKeyParams(params))
+	if err != nil {
+		return nil, err
+	}
+
+	return &register, nil
+}
+
+func (s *presenceService) Update(ctx context.Context, body io.ReadCloser) (*db.UpdatePresenceParams, error) {
+	var dto domain.Presence
+
+	if err := json.NewDecoder(body).Decode(&dto); err != nil {
+		return nil, err
+	}
+
+	params := dto.ToUpdateParams()
+
+	result, err := s.repo.UpdatePresence(ctx, params)
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return nil, err
+	}
+
+	if rows == 0 {
+		return nil, errors.New("not found")
+	}
+
+	return &params, nil
+}
+
 // number_card = ? AND meeting_id = ?
-func (s *presenceService) Delete(ctx context.Context, body io.ReadCloser) (*database.DeletePresenceByCompositeKeyParams, error) {
-	var dto domain.PresenceCompositeKey
+func (s *presenceService) Delete(ctx context.Context, body io.ReadCloser) (*db.DeletePresenceByCompositeKeyParams, error) {
+	var dto domain.PresenceByCompositeKey
 	if err := json.NewDecoder(body).Decode(&dto); err != nil {
 		return nil, err
 	}
