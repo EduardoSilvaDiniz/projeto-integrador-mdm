156392160916aa8ca63e4039fffe80a6ab7cc45b
Eduardo Silva <eduardofox989@protonmail.com>
2025-07-16 14:29:45 -0300

    added: ErrAlreadyExists

--- internal/errs/errs.go
+++ internal/errs/errs.go
@@ -5,7 +5,7 @@ import "errors"
 var (
 	ErrNotFound      = errors.New("registro não encontrado")
 	ErrInvalidInput  = errors.New("dados inválidos")
-	ErrConflict      = errors.New("conflito de dados")
+	ErrAlreadyExists = errors.New("conflito de dados: o registro já está cadastrado")
 	ErrUnauthorized  = errors.New("não autorizado")
 	ErrInternalError = errors.New("erro interno")
 )
